c1e2994786544f55b5d5a520ad62742bfee96a7c
BasanuM <46770030+BasanuM@users.noreply.github.com>
2020-09-08 13:29:03 +0530

    Initial Commit

---
+++ src/app/value/value.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-value',
+  templateUrl: './value.component.html',
+  styleUrls: ['./value.component.css']
+})
+export class ValueComponent implements OnInit {
+
+  values: any;
+   private url = 'http://localhost:5000/Values';
+
+  constructor(private http: HttpClient) { }
+  
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+
+  getValues() {
+    this.http.get(this.url).subscribe(data => {
+      this.values = data;
+    }, error => {
+      console.log(error)
+    });
+  }
+}
